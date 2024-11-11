dddbdfc0d7c05a109b744772d38246deafe67aa3
Mario Hewardt <marioh@microsoft.com>
2024-11-11 14:48:51 -0800

    Update procdump.rb (#2)

--- Formula/procdump.rb
+++ Formula/procdump.rb
@@ -2,9 +2,9 @@ class Procdump < Formula
     desc "ProcDump for Mac provides a convenient way for developers to create core dumps of their application based on performance triggers. ProcDump for Mac is part of Sysinternals."
     homepage "https://learn.microsoft.com/en-us/sysinternals/downloads/procdump"
   
-    url "<change>"  
-    sha256 "<change>"  
-    version "<change>"
+    url "https://github.com/Sysinternals/ProcDump-for-Mac/releases/download/1.0.0/procdump-mac-1.0.0.zip"  
+    sha256 "fd92ce2113dfc0a4d391f9c61aaa9e875a6eba6bc8ac8b142b39e56135820bdd"  
+    version "1.0.0"
     depends_on macos: :sierra  # gcore availability
     license "MIT"
 
